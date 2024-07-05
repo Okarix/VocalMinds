8ae83db2d48b279c5745cc81b317ee06bd418fc4
Adiar <datkaevadiar@gmail.com>
2024-07-05 06:33:04 +0500

    refactor: systemprompt

--- backend/src/analyze/analyze-service.ts
+++ backend/src/analyze/analyze-service.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process';
 import OpenAI from 'openai';
+import { systemPromptJson } from './analyze-type';
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -23,13 +24,15 @@ export async function runPythonScript(filePath: string): Promise<{ outputPath: s
 }
 
 export async function analyzeWithOpenAI(analysisText: string): Promise<object> {
-	const systemPrompt =
-		"Welcome to your personal vocal trainer! I'll help you enhance your voice and improve your singing technique. First, I'll analyze your vocal performance across key parameters: pitch, timbre, dynamics, articulation, rhythm, breath control, and vibrato. Next, I'll provide detailed feedback and personalized recommendations to enhance each aspect of your vocal performance. This may include breathing exercises, vocal warm-ups, and specific technique drills. Let's get started! Please provide your vocal performance for analysis. Return JSON format";
+	const systemPrompt = `Welcome to your personal vocal trainer! I'll help you enhance your voice and improve your singing technique. First, I'll analyze your vocal performance across key parameters: pitch, timbre, dynamics, articulation, rhythm, breath control, and vibrato. Next, I'll provide detailed feedback and personalized recommendations to enhance each aspect of your vocal performance. This may include breathing exercises, vocal warm-ups, and specific technique drills. Let's get started! Please provide your vocal performance for analysis. Without any delimeters such as commas or quotes return JSON format (also do not print in your response's The JSON format should be as follows:"
+		${systemPromptJson}
+		`;
+
 	const userPrompt = analysisText;
 
 	try {
 		const response = await openai.chat.completions.create({
-			model: 'gpt-4',
+			model: 'gpt-4o',
 			messages: [
 				{ role: 'system', content: systemPrompt },
 				{ role: 'user', content: userPrompt },
---
+++ backend/src/analyze/analyze-type.ts
@@ -0,0 +1,32 @@
+export const systemPromptJson = `
+{
+  "pitch": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "timbre": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "dynamics": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "articulation": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "rhythm": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "breath_control": {
+    "rating": "value",
+    "feedback": "value"
+  },
+  "vibrato": {
+    "rating": "value",
+    "feedback": "value"
+  }
+}
+`;
