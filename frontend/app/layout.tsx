fef113188778e3589da3d5bc9241b14eca8a8734
Adiar <datkaevadiar@gmail.com>
2024-08-02 16:18:39 +0500

    feat: add analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter_Tight } from 'next/font/google';
 import './globals.css';
 import { ChatProvider } from '@/context/ChatContext';
 import Header from '@/components/ui/header';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter_Tight({ subsets: ['latin', 'cyrillic'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<ChatProvider>{children}</ChatProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
